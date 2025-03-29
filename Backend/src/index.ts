367fda19a7c49edc489a517e67feaa7bdbba471a
Roshan Eldho Benny <roshannoonoottil@gmail.com>
2025-03-29 13:08:55 +0530

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ limit: "3mb", extended: true }));
 
 
 app.use(cors({
-  origin: "https://infinity-chat-rho.vercel.app", // Frontend URL
+  origin: "*", // Frontend URL
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true // Allow cookies if needed
