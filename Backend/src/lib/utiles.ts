19308e0468c9029f0fb133fb3962252766cb1969
Roshan Eldho Benny <roshannoonoottil@gmail.com>
2025-03-31 15:56:17 +0530

    Update utiles.ts

--- Backend/src/lib/utiles.ts
+++ Backend/src/lib/utiles.ts
@@ -14,8 +14,8 @@ export const generateToken = (userId : String, res : Response) =>{
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, // MS
         httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-        sameSite: "lax", // CSRF attacks cross-site request forgery attacks
-        secure: process.env.NODE_ENV !== "development",
+        sameSite: "none", // Fix cross-site cookie issues
+        secure: true, // Always secure (since your site is HTTPS)
       });
     
       return token;
